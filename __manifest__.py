2ca51bd492a16e480e47ec75482c60411141b209
Jesús Krauer <jesuskrauer@gmail.com>
2018-02-25 13:52:03 -0300

    Se unifica Descripción del módulo con README.md

--- __manifest__.py
+++ __manifest__.py
@@ -6,21 +6,6 @@
 {
     'name': 'Paraguay - Accounting',
     'version': '2.0',
-    'description': """
-Plan de cuentas paraguayo y localizacion de impuestos.
-==================================================
-
-Plan contable paraguayo e impuestos de acuerdo a disposiciones vigentes.
-
-Desarrollado por RAPIDSOFT.
-
-http://www.rapidsoft.com.py
-
-Para utilizar la localización, también deberá descargar el modulo account_parent: http://www.odoo.com/apps/modules/10.0/account_parent/
-
-Para más información, contactarnos en info@rapidsoft.com.py
-
-    """,
     'author': ['Rapidsoft'],
     'website': 'http://www.rapidsoft.com.py',
     'category': 'Localization',
